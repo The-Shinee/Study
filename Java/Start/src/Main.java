fd66d5e0ef24e6bf87b20309e2ba4bd7856621dc
The Shinee <teraburst@gmail.com>
2024-06-05 14:32:26 +0900

    #8 로또 번호 생성기

--- Java/Start/src/Main.java
+++ Java/Start/src/Main.java
@@ -1,93 +1,29 @@
-import java.util.Scanner;
+import java.util.Random;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
-        // 2
-        Scanner scanner = new Scanner(System.in);
-        String operator = scanner.nextLine();
-
-        double num1 = 1, num2 = 2;
-
-        switch (operator) {
-            case "+":
-                System.out.println(num1 + num2);
-                break;
-            case "-":
-                System.out.println(num1 - num2);
-                break;
-            case "*":
-                System.out.println(num1 * num2);
-                break;
-            case "/":
-                System.out.println(num1 / num2);
-                break;
-            default:
-                System.out.println("Error : Input value is : " + operator);
+        Random random = new Random();
+        int[] arr = new int[6];
+        int value;
+        System.out.print("로또 번호 : ");
+        for(int i = 0 ; i < arr.length ; i++){
+            do {
+                value = random.nextInt(45 + 1);
+            } while(isContain(arr, value));
+            arr[i] = value;
+            System.out.printf("[%d] ", arr[i]);
         }
+    }
 
-        // 3
-        String color = scanner.nextLine();
-        String resultColorText = "";
-        switch (color) {
-            case "red":
-                resultColorText = "Apple";
+    static boolean isContain(int[] arr, int value){
+        boolean result = false;
+        for(int v : arr){
+            if(v == value)
+            {
+                result = true;
                 break;
-            case "green":
-                resultColorText = "Grass";
-                break;
-            case "blue":
-                resultColorText = "Sky";
-                break;
-            default:
-                System.out.println("Error : Input value is : " + color);
+            }
         }
-
-        System.out.println(resultColorText);
-
-        int score = scanner.nextInt();
-        char grade = ' ';
-        switch (score / 10) {
-            case 10:
-            case 9:
-                grade = 'A';
-                break;
-            case 8:
-                grade = 'B';
-                break;
-            case 7:
-                grade = 'C';
-                break;
-            case 6:
-                grade = 'D';
-                break;
-            default:
-                grade = 'F';
-        }
-
-        System.out.println(grade);
-
-        String language = scanner.nextLine();
-        String description = "";
-        switch (language) {
-            case "Java":
-                description = "컴파일, 인터프리터 하이브리드 언어로 국내에서 웹 백엔드 개발에 주로 사용";
-                break;
-            case "Python":
-                description = "인터프리터 언어로 AI 머신러닝에 주로 사용";
-                break;
-            case "C++":
-                description = "컴파일 언어로 메모리를 직접 조작할 수 있으며, 임베디드 시스템에 주로 사용";
-                break;
-            case "JavaScript":
-                description = "인터프리터 언어로 웹 개발에 주로 사용";
-            default:
-                System.out.println("Error : Input value is : " + description);
-        }
-
-        System.out.println(description);
-
+        return result;
     }
 }
