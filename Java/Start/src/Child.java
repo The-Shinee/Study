ebfbcb40f895e9d11664e0826b4b14877fffd94e
The Shinee <teraburst@gmail.com>
2024-06-14 17:58:37 +0900

    #13 Override

--- Java/Start/src/Child.java
+++ Java/Start/src/Child.java
@@ -2,4 +2,9 @@ public class Child extends Parent{
     public int withdraw() {
         return setMoney(1000, SUB);
     }
+
+    @Override
+    public int getMoney(){
+        return super.getMoney();
+    }
 }
