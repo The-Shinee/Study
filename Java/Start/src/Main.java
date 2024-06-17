0004d796b5ba3848d281f06a797483edec4fa4b4
The Shinee <teraburst@gmail.com>
2024-06-17 18:17:25 +0900

    #14 ShoppingMall

--- Java/Start/src/Main.java
+++ Java/Start/src/Main.java
@@ -1,13 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Person me = new Person("Cheolan", 28, 1000000);
-        Product earphone = new Product("INZONE Buds", 259000);
-        Product headset = new Product("Inzone H9", 349000);
-        Product keyboard = new Product("Corsair K70", 249000);
 
-        me.BuyProduct(earphone);
-        me.BuyProduct(headset);
-        me.BuyProduct(keyboard);
 
         me.Introduce();
     }
--- Java/Start/src/Product.java
+++ Java/Start/src/Product.java
@@ -1,9 +1,49 @@
 public class Product {
-    public String Name;
-    public int Price;
 
-    public Product(String name, int price){
-        Name = name;
-        Price = price;
+    // region field
+    private String name;
+    private int price;
+    private int stock;
+    // endregion
+
+    public Product(String name, int price, int stock) {
+        setName(name);
+        setPrice(price);
+        setStock(stock);
+    }
+
+    // region Getter, Setter
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+    // endregion
+
+    @Override
+    public String toString() {
+        return "분류: " + getClass().getName() + " 상품명: " + getName() + " 가격: " + calculatePrice() + " 재고: " + getStock();
+    }
+
+    protected int calculatePrice() {
+        return getPrice();
     }
 }
