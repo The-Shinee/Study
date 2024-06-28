f4bb66561b81aa7c882726fda042ece5fe0c81d8
The Shinee <teraburst@gmail.com>
2024-06-28 17:11:19 +0900

    BOJ 10813

---
+++ Java/Start/src/Algorithm/BOJ_10813.java
@@ -0,0 +1,29 @@
+package Algorithm;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ_10813 {
+    public static void Func(){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0 ; i < n ; i++){
+            arr[i] = i + 1;
+        }
+
+        for(int i = 0 ; i < m ; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            int temp = arr[a-1];
+            arr[a-1] = arr[b-1];
+            arr[b-1] = temp;
+        }
+
+        for(int a : arr){
+            System.out.print(a + " ");
+        }
+    }
+}
