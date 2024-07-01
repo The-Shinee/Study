f2e88762c6e58759a590b147f6489e724daab617
The Shinee <teraburst@gmail.com>
2024-07-01 09:34:08 +0900

    BOJ 5597

---
+++ Java/Start/src/Algorithm/BOJ_5597.java
@@ -0,0 +1,24 @@
+package Algorithm;
+import java.util.Scanner;
+
+public class BOJ_5597 {
+    public static void Func(){
+        Scanner sc = new Scanner(System.in);
+
+        int[] arr = new int[30];
+        for(int i = 0 ; i < 30 ; i++){
+            arr[i] = i + 1;
+        }
+
+        for(int i = 0 ; i < 28 ; i++){
+            int a = sc.nextInt();
+            arr[a - 1] = 0;
+        }
+
+        for(int n : arr){
+            if(n != 0){
+                System.out.println(n);
+            }
+        }
+    }
+}
