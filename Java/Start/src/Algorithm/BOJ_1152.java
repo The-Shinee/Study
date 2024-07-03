73808f60d0aae79adb6ba1be98bbd731153bf10b
The Shinee <teraburst@gmail.com>
2024-07-03 18:04:57 +0900

    BOJ 1152

---
+++ Java/Start/src/Algorithm/BOJ_1152.java
@@ -0,0 +1,24 @@
+package Algorithm;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BOJ_1152 {
+    public void Func(){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int count = 0;
+
+        for(int i = 0 ; i < str.length() ; i++){
+            if(str.charAt(i) == ' ' || i == str.length() - 1){
+                continue;
+            }
+
+            if(str.charAt(i + 1) != ' '){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
