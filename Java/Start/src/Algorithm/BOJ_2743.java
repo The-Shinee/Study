7c1cb2359aa722d926d671d0712b672f46ddfc9f
The Shinee <teraburst@gmail.com>
2024-07-08 10:21:32 +0900

    BOJ 2743

---
+++ Java/Start/src/Algorithm/BOJ_2743.java
@@ -0,0 +1,11 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class BOJ_2743 {
+    public void Func(){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.println(str.length());
+    }
+}
