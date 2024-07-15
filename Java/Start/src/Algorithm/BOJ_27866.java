6e0cd2d88a592d8fe94eca83bdefca3ab40de36e
The Shinee <teraburst@gmail.com>
2024-07-15 09:20:14 +0900

    BOJ 27866

---
+++ Java/Start/src/Algorithm/BOJ_27866.java
@@ -0,0 +1,12 @@
+package Algorithm;
+
+import java.util.Scanner;
+
+public class BOJ_27866 {
+    public void Func(){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        int a = sc.nextInt();
+        System.out.println(str.charAt(a - 1));
+    }
+}
